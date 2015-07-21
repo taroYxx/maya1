56692eba106a10c481e7e416ac99817635a2efbd
Taro <Taro@yuxiangxiangdeMacBook-Pro.local>
2015-07-21 15:32:13 +0800

    Initial Commit

---
+++ maya1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  maya1
+//
+//  Created by Taro on 15/7/21.
+//  Copyright (c) 2015年 Taro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
